6f66e251ddcab7c8e00ae5ae811b12d8f4f3549b
Hunter Arthur Gabbard <hunter.gabbard@dgx-1.dcs.ligo-wa.caltech.edu>
2018-09-06 03:10:36 -0700

    fixed bugs

--- simple_nn.py
+++ simple_nn.py
@@ -1,6 +1,6 @@
 import keras
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPool2D,Dense, Activation, Dropout, GaussianDropout, ActivityRegularization, Flatten
+from keras.layers import Conv2D, Reshape, MaxPooling2D, Dense, Activation, Dropout, GaussianDropout, ActivityRegularization, Flatten
 from keras.optimizers import *
 from keras.layers.normalization import BatchNormalization
 from keras import initializers
@@ -33,8 +33,13 @@ def nn_model(input_shape,output_shape):
     model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
                      activation='relu', padding=padding,
                      input_shape=input_shape))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    #model.add(Reshape((output_shape[0],ouput_shape[1])))
+    model.add(MaxPooling2D(pool_size=(2, 1), strides=(2, 1)))
+    #model.add(Reshape((output_shape[0],output_shape[1],32)))
+
+    model.add(Conv2D(64, (5, 5), activation='relu',
+                     padding=padding))
+    model.add(MaxPooling2D(pool_size=(8, 8), strides=(8, 8)))
+
     model.add(Conv2D(1, (5, 5), activation='relu',
                      padding=padding))
     #model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -43,6 +48,7 @@ def nn_model(input_shape,output_shape):
     #model.add(Flatten())
     #model.add(Dense(1000, activation='relu'))
     #model.add(Dense(num_classes, activation='softmax'))
+    model.summary()
 
     return model
 
@@ -98,25 +104,20 @@ def gauss_2d():
 
 def main():
     # define parameters
-    num_train = 10
-    num_val = 10
-    num_test = 10
+    input_shape = [1024,512,1]
+    output_shape = [64,64]
    
-    y_train = []
-    for i in range(num_train):
-        y_train.append(gauss_2d)
+    #y_train = []
+    #for i in range(num_train):
+    #    y_train.append(gauss_2d)
 
-    print(y_train)
-    exit()
-
-
-    
 
     # load data
     
 
     # define model
     model = nn_model(input_shape, output_shape)
+    exit()
 
     # train model
     model, hist, eval_results, preds = run_cnn(model, x_train, y_train, x_val, y_val, x_test, y_test)
